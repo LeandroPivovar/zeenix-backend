41eb52e0c545e1058ea3396bb362c406684e5641
LeandroPivovar <leandropivovar@hotmail.com>
2026-01-20 10:13:37 -0300

    feat

--- src/copy-trading/copy-trading.module.ts
+++ src/copy-trading/copy-trading.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CopyTradingController } from './copy-trading.controller';
 import { CopyTradingService } from './copy-trading.service';
 import { ExpertEntity } from '../infrastructure/database/entities/expert.entity';
+import { UserEntity } from '../infrastructure/database/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ExpertEntity])],
+  imports: [TypeOrmModule.forFeature([ExpertEntity, UserEntity])],
   controllers: [CopyTradingController],
   providers: [CopyTradingService],
   exports: [CopyTradingService],
 })
-export class CopyTradingModule {}
+export class CopyTradingModule { }
 
