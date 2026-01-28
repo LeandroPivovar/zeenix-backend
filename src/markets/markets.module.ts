cef12b2cc44bc57665ea846844989c35b8e83012
LeandroPivovar <leandropivovar@hotmail.com>
2026-01-28 04:08:15 -0300

    feat

--- src/markets/markets.module.ts
+++ src/markets/markets.module.ts
@@ -4,8 +4,10 @@ import { MarketsService } from './markets.service';
 import { MarketsController } from './markets.controller';
 import { MarketEntity } from '../infrastructure/database/entities/market.entity';
 
+import { MarketContractEntity } from '../infrastructure/database/entities/market-contract.entity';
+
 @Module({
-    imports: [TypeOrmModule.forFeature([MarketEntity])],
+    imports: [TypeOrmModule.forFeature([MarketEntity, MarketContractEntity])],
     controllers: [MarketsController],
     providers: [MarketsService],
     exports: [MarketsService],
